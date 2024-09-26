203e58cce7ba1fdde11afdf07d9f3e774ba5069c
junhyun bang <flintintoe@gmail.com>
2024-09-27 00:34:04 +0900

    Add Problem (729.py)

---
+++ dailychallenge/729.py
@@ -0,0 +1,87 @@
+class MyCalendar:
+
+    def __init__(self):
+        self.intervals = []
+
+    def book(self, start: int, end: int) -> bool:
+        passed = True
+        size = len(self.intervals)
+
+        # base case
+        if size == 0:
+            self.intervals.append(start)
+            self.intervals.append(end)
+
+            return True
+
+        i = self.binary_search(start)
+        #print('i =', i)
+        j = self.binary_search(end)
+        #print('j =', j)
+
+        if abs(i - j) > 1:
+            return False
+
+        # test start
+        if not i == size:
+            if i % 2 == 0: # is a start
+                starting_at = self.intervals[i]
+
+                passed = passed and (start < starting_at)
+                    
+            else: # is an end
+                ending_at = self.intervals[i]
+
+                passed = passed and (start >= ending_at)
+           
+
+        # test end
+        if not j == size:
+            if j % 2 == 0: # is a start
+                started = self.intervals[j]
+
+                passed = passed and (end <= started)
+            else: # is an end
+                ended = self.intervals[j]
+
+                passed = passed and (end > ended)
+        
+        if passed:
+            self.intervals.insert(i, start)
+            self.intervals.insert(j + 1, end)
+
+            #print(self.intervals)
+
+        return passed
+
+    def binary_search(self, target: int) -> int:
+        low = 0
+        high = len(self.intervals) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            mid_val = self.intervals[mid]
+
+            # print("mid=", mid)
+
+            if mid_val == target:
+                return mid
+            elif mid_val < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return low; # return the last successful search index
+
+
+
+        
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
+obj = MyCalendar()
+print(obj.book(10,20))
+print(obj.book(15,25))
+print(obj.book(20,30))
